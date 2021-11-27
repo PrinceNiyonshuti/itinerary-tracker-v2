6c36f677904bacc0f27170e72cac5aa562f66d7b
PrinceNiyonshuti <npprince47@gmail.com>
2021-11-27 10:37:53 +0200

    added textarea component

--- src/components/TexArea.tsx
+++ src/components/TexArea.tsx
@@ -1,9 +1,25 @@
 /** @format */
 
-import React from "react";
+type textProp = {
+	id: string;
+	rows: number;
+	placeholder: string;
+	refData?: any;
+};
 
-function TexArea() {
-	return <div></div>;
-}
+const TextArea = ({ id, rows, refData, placeholder }: textProp) => {
+	return (
+		<div className=" p-3 w-full">
+			<textarea
+				id={id}
+				rows={rows}
+				required
+				ref={refData}
+				className="border p-2 rounded w-full"
+				placeholder={placeholder}
+			/>
+		</div>
+	);
+};
 
-export default TexArea;
+export default TextArea;
--- src/pages/AddItinerary.tsx
+++ src/pages/AddItinerary.tsx
@@ -3,6 +3,7 @@
 import React, { useContext } from "react";
 import Button from "../components/Button";
 import Input from "../components/Input";
+import TextArea from "../components/TexArea";
 
 const AddItenary = () => {
 	return (
@@ -16,6 +17,12 @@ const AddItenary = () => {
 						<Input type={"date"} id={"date"} placeholder={""} />
 
 						<Input type={"time"} id={"time"} placeholder={""} />
+
+						<TextArea
+							id={"description"}
+							rows={3}
+							placeholder={"Write something..."}
+						/>
 
 						<div className="flex justify-between mx-3">
 							<div>
