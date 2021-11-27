d65f54efe06aff08c8f59ab0755e16db41bd8776
PrinceNiyonshuti <npprince47@gmail.com>
2021-11-27 10:48:02 +0200

    added all Itinarary page

---
+++ src/pages/AllItinerary.tsx
@@ -0,0 +1,18 @@
+/** @format */
+
+import React, { useContext } from "react";
+import Itenaries from "../components/Itenaries";
+
+function AllItinerary() {
+
+	return (
+		<div>
+			<div className="w-full p-4">
+				<ul>
+				</ul>
+			</div>
+		</div>
+	);
+}
+
+export default AllItinerary;
