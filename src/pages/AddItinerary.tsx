d472857ff44a5a35f9862cbfadea487f28c5c5f4
PrinceNiyonshuti <npprince47@gmail.com>
2021-11-27 13:47:59 +0200

    Creating Priority Done

--- src/pages/AddItinerary.tsx
+++ src/pages/AddItinerary.tsx
@@ -1,24 +1,71 @@
 /** @format */
-
+import React, { useRef } from "react";
 import Button from "../components/Button";
 import Input from "../components/Input";
 import TextArea from "../components/TexArea";
 
 const AddItenary = () => {
+	// Form variables
+	const itenaryTitle = useRef<HTMLInputElement>(null);
+	const itenaryDate = useRef<HTMLInputElement>(null);
+	const itenaryTime = useRef<HTMLInputElement>(null);
+	const itenaryDescription = useRef<HTMLInputElement>(null);
+	const itenaryForm = useRef<HTMLFormElement>(null);
+
+	const newItenary = () => {
+		// event.preventDefault();
+
+		const title = itenaryTitle.current?.value;
+		const date = itenaryDate.current?.value;
+		const time = itenaryTime.current?.value;
+		const description = itenaryDescription.current?.value;
+		const priority = false;
+
+		const itinerary = { title, date, time, description, priority };
+		console.log(itinerary);
+
+		fetch("http://localhost:8000/itenaryList", {
+			method: "POST",
+			headers: { "Content-Type": "application/json" },
+			body: JSON.stringify(itinerary),
+		}).then(() => {
+			console.log("New Itinerary added");
+			window.location.reload();
+		});
+
+		// itenaryForm.current?.reset();
+	};
+
 	return (
 		<div>
 			<h3 className="font-bold text-lg mb-1 mx-5">New Itenary</h3>
-			<form>
+			<form onSubmit={newItenary} ref={itenaryForm}>
 				<div className="flex">
 					<div className="w-full bg-white p-2 rounded">
-						<Input type={"text"} id={"title"} placeholder={"Itinerary Title"} />
+						<Input
+							type={"text"}
+							refData={itenaryTitle}
+							id={"title"}
+							placeholder={"Itinerary Title"}
+						/>
 
-						<Input type={"date"} id={"date"} placeholder={""} />
+						<Input
+							type={"date"}
+							refData={itenaryDate}
+							id={"date"}
+							placeholder={""}
+						/>
 
-						<Input type={"time"} id={"time"} placeholder={""} />
+						<Input
+							type={"time"}
+							refData={itenaryTime}
+							id={"time"}
+							placeholder={""}
+						/>
 
 						<TextArea
 							id={"description"}
+							refData={itenaryDescription}
 							rows={3}
 							placeholder={"Write something..."}
 						/>
