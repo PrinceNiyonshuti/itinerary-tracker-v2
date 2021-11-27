236b5ef9d71525fb63f4cab8539d90a6ea6c176e
PrinceNiyonshuti <npprince47@gmail.com>
2021-11-27 14:59:30 +0200

    last push on read details

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -25,25 +25,7 @@ const Home = () => {
 				"Curabitur aliquet quam id dui posuere blandit. Pellentesque in ipsum id orci porta dapibus. Proin eget tortor risus. Curabitur aliquet quam id dui posuere blandit. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Proin eget tortor risus. Pellentesque in ipsum id orci porta dapibus. Curabitur aliquet quam id dui posuere blandit. Nulla quis lorem ut libero malesuada feugiat. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a.",
 			priority: true,
 			id: 1,
-		},
-		{
-			title: "Do laundry",
-			date: "2021-11-07",
-			time: "08:00",
-			description:
-				"Read the Labels. Check the care labels on your garments and linens. ...\nSort. Start by sorting the laundry by color: ...\nSort Again. Sort each pile one more time by type of fabric. ...\nPick a Detergent. ...\nPick a Water Temperature and Cycle. ...\nFinal Check. ...\nLoad the Washer. ...\nUnload the Washer.",
-			priority: false,
-			id: 2,
-		},
-		{
-			title: "Go to gym",
-			date: "2021-11-06",
-			time: "18:00",
-			description:
-				"What is a gym? A gym - physical exercises and activities performed inside, often using equipment, especially when done as a subject at school. Gymnasium is a large room with equipment for exercising the body and increasing strength or a club where you can go to exercise and keep fit.",
-			priority: false,
-			id: 3,
-		},
+		}
 	];
 	const [itenaryData, setItenaryData] = useState<IState["itenary"]>(itenaryList);
 
--- src/pages/ReadItinerary.tsx
+++ src/pages/ReadItinerary.tsx
@@ -1,27 +1,52 @@
 /** @format */
-
+import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+
+// Type | Interface
+type IState = {
+	itenary: {
+		title: string;
+		date: string;
+		time: string;
+		description: string;
+		priority: boolean;
+		id: number;
+	}[];
+};
 
 function ReadItinerary() {
 	const { id } = useParams();
+
+	const itenaryList = [
+		{
+			title: "Read React documentation",
+			date: "2021-11-07",
+			time: "10:24",
+			description:
+				"Curabitur aliquet quam id dui posuere blandit. Pellentesque in ipsum id orci porta dapibus. Proin eget tortor risus. Curabitur aliquet quam id dui posuere blandit. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Proin eget tortor risus. Pellentesque in ipsum id orci porta dapibus. Curabitur aliquet quam id dui posuere blandit. Nulla quis lorem ut libero malesuada feugiat. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a.",
+			priority: true,
+			id: 1,
+		},
+	];
+	const [itenaryData, setItenaryData] =
+		useState<IState["itenary"]>(itenaryList);
+
+	useEffect(() => {
+		fetch(`http://localhost:8000/itenaryList/` + id)
+			.then((res) => {
+				return res.json();
+			})
+			.then((data) => {
+				setItenaryData(data);
+			});
+		// eslint-disable-next-line
+	}, []);
+
+	console.log(itenaryData);
 	return (
 		<div className="mx-5">
-			<div>
-				<h3 className="font-bold text-2xl my-2">{id}</h3>
-				<hr />
-				<p className="font-normal my-2">
-					<b>Day</b> :
-					<br />
-					<b>Time</b> :
-					<br />
-					<span className="bg-green-600 text-white text-sm px-3 py-1 rounded-full">
-						✌🏻 Priority
-					</span>
-				</p>
-
-				<p className="font-normal"></p>
-				<p className="font-normal text-justify my-2">Descrpiton</p>
-			</div>
+			{id}
+			<p>Test Data</p>
 		</div>
 	);
 }
