1f185294c853151001c23d279bad1236eed8ee87
PrinceNiyonshuti <npprince47@gmail.com>
2021-11-27 12:22:39 +0200

    Getting Itineraries Done for API call

--- src/pages/AllItinerary.tsx
+++ src/pages/AllItinerary.tsx
@@ -1,10 +1,50 @@
 /** @format */
 
+import { useState, useEffect } from "react";
+import Itinerary from "../components/Itinerary";
+
+// Type | Interface
+type IState = {
+	itenary: {
+		title: string;
+		date: string;
+		time: string;
+		description: string;
+		priority: boolean;
+		id: number;
+	}[];
+};
 function AllItinerary() {
+	const itenaries = [
+		{
+			title: "Read React documentation",
+			date: "2021-11-07",
+			time: "10:24",
+			description:
+				"Curabitur aliquet quam id dui posuere blandit. Pellentesque in ipsum id orci porta dapibus. Proin eget tortor risus. Curabitur aliquet quam id dui posuere blandit. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Proin eget tortor risus. Pellentesque in ipsum id orci porta dapibus. Curabitur aliquet quam id dui posuere blandit. Nulla quis lorem ut libero malesuada feugiat. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a.",
+			priority: true,
+			id: 1,
+		}
+	];
+	// Retrieve all Itenerary
+	useEffect(() => {
+		fetch(`http://localhost:8000/itenaryList`)
+			.then((res) => {
+				return res.json();
+			})
+			.then((data) => {
+				setItenaryAllData(data);
+			});
+	}, []);
+
+	const [itenaryAllData, setItenaryAllData] = useState<IState["itenary"]>(itenaries);
+
 	return (
 		<div>
 			<div className="w-full p-4">
-				<ul></ul>
+				<ul>
+					<Itinerary itenaryData={itenaryAllData} />
+				</ul>
 			</div>
 		</div>
 	);
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,20 @@
 /** @format */
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import Itinerary from "../components/Itinerary";
+
+// Type | Interface
+type IState = {
+	itenary: {
+		title: string;
+		date: string;
+		time: string;
+		description: string;
+		priority: boolean;
+		id: number;
+	}[];
+};
+
 const Home = () => {
 	const itenaryList = [
 		{
@@ -31,11 +45,24 @@ const Home = () => {
 			id: 3,
 		},
 	];
+	const [itenaryData, setItenaryData] = useState<IState["itenary"]>(itenaryList);
+
+	// Retrieve all Itenerary with limit of 5
+	useEffect(() => {
+		fetch(`http://localhost:8000/itenaryList/?_limit=5`)
+			.then((res) => {
+				return res.json();
+			})
+			.then((data) => {
+				setItenaryData(data);
+			});
+	}, []);
+
 	return (
 		<div>
 			<div className="w-full p-4">
 				<ul>
-					<Itinerary itenaryData={itenaryList} />
+					<Itinerary itenaryData={itenaryData} />
 				</ul>
 				<Link to="/all">
 					<p className="font-normal text-blue-700 text-center mt-2">
