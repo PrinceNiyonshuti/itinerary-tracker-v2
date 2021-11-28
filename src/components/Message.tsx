de83ddb70c32f772b74ad61b3dd94a5f2d3a120b
PrinceNiyonshuti <npprince47@gmail.com>
2021-11-28 21:25:24 +0200

    listing messages done

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -1,11 +1,34 @@
-import React from 'react'
+/** @format */
 
-function Message() {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+// Type | Interface
+type messageProps = {
+	contactData: {
+		username: string;
+		email: string;
+		description: string;
+		id: number;
+	}[];
+};
 
-export default Message
+const Message = (props: messageProps) => {
+	console.log(props.contactData);
+	return (
+		<div>
+			{props.contactData.map((message) => (
+				<li
+					key={message.id}
+					className="flex justify-between items-center border-b-2 border-green-200 mt-2 p-2 cursor-pointer transition">
+					<div className="flex ml-2">
+						<div className="flex flex-col ml-2">
+							<p className="font-semibold text-xl">From : {message.username}</p>
+							<span className="text-sm">@{message.email}</span>
+							<p className=" font-normal text-justify">{message.description}</p>
+						</div>
+					</div>
+				</li>
+			))}
+		</div>
+	);
+};
+
+export default Message;
--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,10 +1,36 @@
 /** @format */
-import { useRef } from "react";
+import { useRef, useState, useEffect } from "react";
 import Button from "../components/Button";
 import Input from "../components/Input";
+import Message from "../components/Message";
 import TextArea from "../components/TexArea";
 
+// Type | Interface
+type contactState = {
+	message: {
+		username: string;
+		email: string;
+		description: string;
+		id: number;
+	}[];
+};
+
 function Contact() {
+	// Defailt data
+
+	const messageData = [
+		{
+			username: "Debby ",
+			email: "debby@gmail.com",
+			description:
+				"Donec rutrum congue leo eget malesuada. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec velit neque, auctor sit amet aliquam vel, ullamcorper sit amet ligula.",
+			id: 1,
+		},
+	];
+
+	const [contactData, setContactData] =
+		useState<contactState["message"]>(messageData);
+
 	// Contact Form variables
 	const contactForm = useRef<HTMLFormElement>(null);
 	const contactNames = useRef<HTMLInputElement>(null);
@@ -15,12 +41,12 @@ function Contact() {
 	// Function to send message
 	const SendMessage = () => {
 		// Assigning Form Data
-		const userNames = contactNames.current?.value;
+		const username = contactNames.current?.value;
 		const email = contactEmail.current?.value;
 		const subject = contactSubject.current?.value;
 		const description = contactMessage.current?.value;
 
-		const contactData = { userNames, email, subject, description };
+		const contactData = { username, email, subject, description };
 
 		// Posting Contact Message Data to Json file
 		fetch("http://localhost:8000/messageList/", {
@@ -29,12 +55,23 @@ function Contact() {
 			body: JSON.stringify(contactData),
 		}).then(() => {
 			console.log("New Message Sent");
-			alert(`Dear ${userNames} , Your Message Is Sent Successfully`);
+			alert(`Dear ${username} , Your Message Is Sent Successfully`);
 			window.location.reload();
 		});
 
 		// contactForm.current?.reset();
 	};
+
+	// Retrieve all Message list
+	useEffect(() => {
+		fetch(`http://localhost:8000/messageList`)
+			.then((res) => {
+				return res.json();
+			})
+			.then((data) => {
+				setContactData(data);
+			});
+	}, []);
 
 	return (
 		<div className="mx-5">
@@ -47,7 +84,7 @@ function Contact() {
 					<div className="w-full bg-white p-2 rounded">
 						<Input
 							type={"text"}
-							id={"userNames"}
+							id={"username"}
 							refData={contactNames}
 							placeholder={"Your Names"}
 						/>
@@ -78,6 +115,7 @@ function Contact() {
 				</div>
 			</form>
 			<h3 className="font-bold text-2lg my-2">Recent Messages</h3>
+			<Message contactData={contactData} />
 		</div>
 	);
 }
